bb319a4a1d6063e028c411d14e4fe5dae123ae90
Stephanie Xue <stephaniexue3@gmail.com>
2026-01-17 13:48:00 -0800

    fix gitignore

--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_hackathon_page_actions_8d63e98c.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/hackathon/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_hackathon_page_actions_8d63e98c.js.map
--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OCw8Lx9Kq6Y7htewIGslsMvJnACQxZNGN6Ls+tjoBSc=\"\n}"
