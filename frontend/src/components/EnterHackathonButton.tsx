6d4fcda71e8bc20087fa86c51ddd71b141fcd4b6
Yingyu <yingyulam@gmail.com>
2026-01-17 16:23:26 -0800

    Made a form for user information

--- frontend/src/components/EnterHackathonButton.tsx
+++ frontend/src/components/EnterHackathonButton.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useState } from "react";
+import PopupForm from "./PopupForm";
 
 export default function EnterHackathonButton() {
-  const router = useRouter();
 
-  const handleEnter = () => {
-    router.push("/hackathon");
-  };
-
-  return (
-    <button
-      onClick={handleEnter}
-      className="px-6 py-3 rounded-lg border border-black bg-black hover:bg-gray-500 hover:text-white transition"
-    >
-      Enter Hackathon
-    </button>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+  
+    return (
+      <>
+        <button
+          onClick={() => setIsOpen(true)}
+          className="px-6 py-3 rounded-lg border border-black bg-black hover:bg-gray-500 hover:text-white transition"
+        >
+          Enter Hackathon
+        </button>
+  
+        {isOpen && <PopupForm onClose={() => setIsOpen(false)} />}
+      </>
+    );
 }
